f17f355e68278ba6a9e8929b0f474e8faf28a88d
Simon Risberg <sirsimon40@gmail.com>
2021-09-25 15:31:34 +0200

    Created action creators for all actions

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,9 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveTweets } from './tweets'
+import { setAuthedUser } from './authedUser'
+
+const AUTHED_ID = 'tylermcginnis'
 
 export function handleInitialData() {
   return (dispatch) => {
@@ -8,6 +11,7 @@ export function handleInitialData() {
       .then(({users, tweets}) => { 
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
+        dispatch(setAuthedUser(AUTHED_ID))
       })
   }
 }
