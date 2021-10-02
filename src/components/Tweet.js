fd7f5aced609adf2637b2754967a0ba60825d65e
Simon Risberg <sirsimon40@gmail.com>
2021-10-02 12:30:05 +0200

    Added logic to see if a tweet has a parent tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,12 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
   render() {
+    const { tweet } = this.props
+    
+    if (tweet === null) {
+      return <p>This tweet doesn't exist</p>
+    }
+
     console.log(this.props)
     return (
       <div className='tweet'>
@@ -15,10 +21,11 @@ class Tweet extends Component {
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweet[tweet.replyingTo] : null
 
   return {
      authedUser,
-     tweet: formatTweet(tweet, users[tweet.author], authedUser)
+     tweet: tweet ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet) : null
   }
 }
 
