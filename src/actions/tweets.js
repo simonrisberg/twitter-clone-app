975a32f27ccbcdc6fe9aec1a08d1ce7c6951543e
Simon Risberg <sirsimon40@gmail.com>
2021-10-03 11:32:00 +0200

    Added a like functionality so users can like tweets

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,9 +1,33 @@
+import { saveLikeToggle } from '../utils/api'
 
 export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
 
 export function receiveTweets(tweets) {
   return {
     type: RECEIVE_TWEETS,
     tweets,
+  }
+}
+
+function toggleTweet({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked,
+  }
+}
+
+export function handleToggleTweet(tweetInfo) {
+  return (dispatch) => {
+    dispatch(toggleTweet(tweetInfo))
+
+    return saveLikeToggle(tweetInfo)
+      .catch((e) => {
+        console.warn("Error in handleToggleTweet: ", e)
+        dispatch(toggleTweet(tweetInfo))
+        alert("There was an error liking the tweet. Try again.")
+      })
   }
 }
