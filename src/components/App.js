4dfa14add26b583925ec2d8577b6d61bf47f63ce
Simon Risberg <sirsimon40@gmail.com>
2021-10-05 22:15:40 +0200

    Created NewTweet component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import LoadingBar from 'react-redux-loading'
 import Dashboard from './Dashboard'
+import NewTweet from './NewTweet'
 
 class App extends Component {
   componentDidMount() {
@@ -14,7 +15,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <Dashboard />}
+          : <NewTweet />}
       </div>
     )
   }
