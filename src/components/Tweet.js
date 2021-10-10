f1e5ec64c4d5475c1c883d0f92fff2ebde673532
Simon Risberg <sirsimon40@gmail.com>
2021-10-10 14:44:28 +0200

    Functionality that lets the user press replying to on a tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from 'react-icons/ti'
 import { TiHeartOutline } from 'react-icons/ti'
 import { TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -21,7 +22,9 @@ class Tweet extends Component {
 
   toParent = (e, id) => {
     e.preventDefault()
-    //todo: Redirect to parent tweet
+
+    this.props.history.push(`/tweet/${id}`)
+    
   }
 
   render() {
@@ -31,10 +34,10 @@ class Tweet extends Component {
       return <p>This tweet doesn't exist</p>
     }
 
-    const { name, avatar, timestamp, text, hasLiked, likes, replies, parent } = tweet
+    const { name, avatar, timestamp, text, hasLiked, likes, replies, id, parent } = tweet
 
     return (
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img src={avatar} alt={`Avatar of ${name}`} className='avatar' />
         <div className='tweet-info'>
           <div>
@@ -59,14 +62,14 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
 
 function mapStateToProps({ authedUser, users, tweets }, { id }) {
   const tweet = tweets[id]
-  const parentTweet = tweet ? tweet[tweet.replyingTo] : null
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
   return {
     authedUser,
@@ -74,4 +77,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
